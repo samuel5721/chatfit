fe1f0fdca15c3c1f2be27f6f4414b7d1d01ca2c7
samuel5721 <samuel20070731@gmail.com>
2024-05-15 09:31:04 +0900

    feat: image pick and upload firebase

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 
 class CameraScreen extends StatefulWidget {
@@ -10,50 +12,62 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
-  XFile? _image; //이미지를 담을 변수 선언
-  final ImagePicker picker = ImagePicker(); //ImagePicker 초기화
+  XFile? _image; // 이미지를 담을 변수 선언
+  final ImagePicker picker = ImagePicker(); // ImagePicker 초기화
+  bool _isUploading = false; // 업로드 상태를 추적하는 변수
 
-  //이미지를 가져오는 함수
+  // 이미지를 가져오는 함수
   Future getImage(ImageSource imageSource) async {
-    //pickedFile에 ImagePicker로 가져온 이미지가 담긴다.
+    // pickedFile에 ImagePicker로 가져온 이미지가 담긴다.
     final XFile? pickedFile = await picker.pickImage(source: imageSource);
     if (pickedFile != null) {
       setState(() {
-        _image = XFile(pickedFile.path); //가져온 이미지를 _image에 저장
+        _image = XFile(pickedFile.path); // 가져온 이미지를 _image에 저장
       });
     }
   }
 
-  // Future uploadImage() async {
-  //   if (_image != null) {
-  //     // 'images/fileName.jpg'에 참조 생성
-  //     var fileRef = FirebaseStorage.instance
-  //         .ref()
-  //         .child('images/${DateTime.now().millisecondsSinceEpoch}.jpg');
+  // 이미지를 Firebase Storage에 업로드하는 함수
+  Future<void> _uploadImage() async {
+    if (_image == null) return;
 
-  //     // 파일 업로드
-  //     await fileRef.putFile(File(_image!.path));
+    setState(() {
+      _isUploading = true; // 업로드 상태를 true로 설정
+    });
 
-  //     // 파일 url 가져오기
-  //     String downloadURL = await fileRef.getDownloadURL();
-  //     print('Uploaded! URL: $downloadURL');
-  //   }
-  // }
+    File file = File(_image!.path);
+    try {
+      await FirebaseStorage.instance
+          .ref('uploads/${file.path.split('/').last}')
+          .putFile(file);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('File uploaded successfully')),
+      );
+    } on FirebaseException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to upload file: ${e.message}')),
+      );
+    } finally {
+      setState(() {
+        _isUploading = false; // 업로드 상태를 false로 설정
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Camera Screen")),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 30, width: double.infinity),
-            _buildPhotoArea(),
-            const SizedBox(height: 20),
-            _buildButton(),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Camera Screen")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 30, width: double.infinity),
+          _buildPhotoArea(),
+          const SizedBox(height: 20),
+          _buildButton(),
+          const SizedBox(height: 20),
+          _buildUploadButton(),
+        ],
       ),
     );
   }
@@ -63,11 +77,11 @@ class _CameraScreenState extends State<CameraScreen> {
         ? SizedBox(
             width: 200,
             height: 200,
-            child: Image.file(File(_image!.path)), //가져온 이미지를 화면에 띄워주는 코드
+            child: Image.file(File(_image!.path)), // 가져온 이미지를 화면에 띄워주는 코드
           )
         : Container(
-            width: 300,
-            height: 300,
+            width: 200,
+            height: 200,
             color: Colors.grey,
           );
   }
@@ -78,18 +92,29 @@ class _CameraScreenState extends State<CameraScreen> {
       children: [
         ElevatedButton(
           onPressed: () {
-            getImage(ImageSource.camera); //getImage 함수를 호출해서 카메라로 찍은 사진 가져오기
+            getImage(ImageSource.camera); // getImage 함수를 호출해서 카메라로 찍은 사진 가져오기
           },
           child: const Text("카메라"),
         ),
         const SizedBox(width: 30),
         ElevatedButton(
           onPressed: () {
-            getImage(ImageSource.gallery); //getImage 함수를 호출해서 갤러리에서 사진 가져오기
+            getImage(ImageSource.gallery); // getImage 함수를 호출해서 갤러리에서 사진 가져오기
           },
           child: const Text("갤러리"),
         ),
       ],
     );
+  }
+
+  Widget _buildUploadButton() {
+    return _image != null
+        ? _isUploading
+            ? CircularProgressIndicator()
+            : ElevatedButton(
+                onPressed: _uploadImage,
+                child: const Text("업로드하기"),
+              )
+        : Container();
   }
 }
