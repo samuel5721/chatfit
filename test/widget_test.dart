3f04e6a52138487df53c941a0c7219c924bafd2f
samuel5721 <samuel20070731@gmail.com>
2024-07-17 20:07:14 +0900

    fix: bug fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:second_application/main.dart';
+import 'package:chatfit/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
